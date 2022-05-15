fc7b0278357ce64d2193ef9c750cc3f19dca685d
Rewen <evgeniy.movchan.03@gmail.com>
2022-05-15 18:26:34 +1000

    Add hero move function

--- movchan.ee/RogueLIKE/RogueLIKE/Program.cs
+++ movchan.ee/RogueLIKE/RogueLIKE/Program.cs
@@ -17,7 +17,7 @@ namespace RogueLIKE
     public int Damage;
     public int Health;
   }
-  
+
   static class Program
   {
     static char[,] CreatMap(int sizeMap)
@@ -29,17 +29,18 @@ namespace RogueLIKE
         map[i, 0] = '#';
         map[i, sizeMap - 1] = '#';
         map[sizeMap - 1, i] = '#';
-        if (i > 0 && i < sizeMap-1)
+        if (i > 0 && i < sizeMap - 1)
         {
-          for (int j = 1; j < sizeMap-1; j++)
+          for (int j = 1; j < sizeMap - 1; j++)
           {
             map[i, j] = '.';
           }
         }
-        map[sizeMap-2, sizeMap-1] = ':';
+        map[sizeMap - 2, sizeMap - 1] = ':';
       }
       return map;
     }
+
     static void PrintMap(int sizeMap, char[,] map)
     {
       for (int i = 0; i < sizeMap; i++)
@@ -51,6 +52,7 @@ namespace RogueLIKE
         Console.WriteLine();
       }
     }
+
     static Hero CreateHeroCharacter(int sizeMap)
     {
       Hero mainHero = new Hero();
@@ -62,18 +64,52 @@ namespace RogueLIKE
       mainHero.IsHero = true;
       return mainHero;
     }
+
     static Hero CreateMonsterCharacter(int sizeMap)
     {
       var rand = new Random();
       Hero monster = new Hero();
-      monster.x = rand.Next(2, sizeMap-2);
-      monster.y = rand.Next(2, sizeMap-2);
+      monster.x = rand.Next(2, sizeMap - 2);
+      monster.y = rand.Next(2, sizeMap - 2);
       monster.Health = rand.Next(1, 7);
       monster.Damage = rand.Next(1, 5);
       monster.sign = 'M';
       monster.IsHero = false;
       return monster;
     }
+
+    static Hero MotionHero(int sizeMap , Hero mainHero)
+    {
+      ConsoleKeyInfo key;
+      key = Console.ReadKey(true);
+      int y = mainHero.y, x = mainHero.x;
+
+      switch (key.Key)
+      {
+        case ConsoleKey.DownArrow:
+          x += 1;
+          if ((x <= (sizeMap - 2)) && x >= 1)
+            mainHero.x += 1;
+          break;
+        case ConsoleKey.UpArrow:
+          x -= 1;
+          if ((x <= (sizeMap - 2)) && x >= 1)
+            mainHero.x -= 1;
+          break;
+        case ConsoleKey.RightArrow:
+          y += 1;
+          if ((y <= (sizeMap - 2)) && y >= 1)
+            mainHero.y += 1;
+          break;
+        case ConsoleKey.LeftArrow:
+          y -= 1;
+          if ((y <= (sizeMap - 2)) && y >= 1)
+            mainHero.y -= 1;
+          break;
+      }
+      return mainHero;
+    }
+
     static void Main()
     {
       var rand = new Random();
@@ -90,10 +126,16 @@ namespace RogueLIKE
        
       Hero mainHero = CreateHeroCharacter(sizeMap);
       map[mainHero.x, mainHero.y] = mainHero.sign;
-      
-      
-
       PrintMap(sizeMap, map);
+      
+      while (true)
+      {
+        map[mainHero.x, mainHero.y] = '.';
+        mainHero = MotionHero(sizeMap, mainHero);
+      
+        map[mainHero.x, mainHero.y] = mainHero.sign;
+        PrintMap(sizeMap, map);
+      }
     }
 
   }
